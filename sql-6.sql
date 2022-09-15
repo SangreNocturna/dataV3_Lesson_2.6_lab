c2037daafb5450bfb847bfd777cab94644af810b
SangreNocturna <jtafernandez@gmail.com>
2022-09-15 18:56:40 +0200

    sql-6.sql

--- sql-6.sql
+++ sql-6.sql
@@ -8,10 +8,34 @@ GROUP BY last_name;
 select distinct first_name, last_name from sakila.actor;
  
 -- 3. Using the rental table, find out how many rentals were processed by each employee
-select * from sakila.rental;
 select staff_id, count(rental_id) from sakila.rental
 group by staff_id;
  
 -- 4. Using the film table, find out how many films were released each year
+select release_year, count(title) from sakila.film
+group by release_year;
  
- 
+-- 5. Using the film table, find out for each rating how many films were there
+select rating, count(title) from sakila.film
+group by rating;
+
+-- 6. What is the mean length of the film for each rating type. Round off the average lengths to two decimal places
+select rating, count(title), round(avg(length),2) as average_length from sakila.film
+group by rating;
+
+-- 7. Which kind of movies (rating) have a mean duration of more than two hours?
+select * from sakila.film;
+select rating, count(title), round(avg(length),2) as average_length from sakila.film
+where length > 120
+group by rating;
+
+-- 8. Rank films by length (filter out the rows that have nulls or 0s in length column). In your output, only select the columns title, length, and the rank
+
+select title, length, rating from sakila.film
+where (title IS NOT NULL) and (length IS NOT NULL) and (rating IS NOT NULL)
+group by length;
+
+
+
+
+
