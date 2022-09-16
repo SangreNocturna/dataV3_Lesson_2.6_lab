8148f9d37b8972994c4a39320bba2868cfd5ec90
SangreNocturna <jtafernandez@gmail.com>
2022-09-16 15:30:18 +0200

    sql-6.sql

--- sql-6.sql
+++ sql-6.sql
@@ -1,11 +1,13 @@
 use sakila;
 
 -- 1. In the table actor, which are the actors whose last names are not repeated? For example if you would sort the data in the table actor by last_name, you would see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. So we do not want to include this last name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", hence we would want this in our output list
-select first_name, last_name from sakila.actor
-GROUP BY last_name;
+select first_name, last_name, count(*) from sakila.actor
+GROUP BY last_name
+having count(*)=1;
  
 -- 2. Which last names appear more than once? We would use the same logic as in the previous question but this time we want to include the last names of the actors where the last name was present more than once
-select distinct first_name, last_name from sakila.actor;
+select distinct first_name, last_name, count(*) from sakila.actor
+having count(*)>1;
  
 -- 3. Using the rental table, find out how many rentals were processed by each employee
 select staff_id, count(rental_id) from sakila.rental
